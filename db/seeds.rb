8074aae420fe9012f5f04c767a266ff17588e135
giacomo montemurri <gia94.montemurri@yahoo.it>
2019-03-06 16:52:55 +0100

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '09393890982',
+    category: "belgian"
+  },
+  {
+    name: 'Dishoo',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '09393890982',
+    category: "belgian"
+  },
+  {
+    name: 'Disho',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '09393890982',
+    category: "belgian"
+  },
+  {
+    name: 'Dish',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '09393890982',
+    category: "belgian"
+  },
+  {
+    name: 'Dis',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '09393890982',
+    category: "belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
